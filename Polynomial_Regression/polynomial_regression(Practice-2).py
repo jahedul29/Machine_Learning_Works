315902dd2894bf0e9aac7942f89a352eb16db8e7
jahedul29 <jahedulh1@gmail.com>
2020-02-19 11:52:31 +0600

    Predicting salary using polynomial regression

--- Polynomial_Regression/polynomial_regression(Practice-2).py
+++ Polynomial_Regression/polynomial_regression(Practice-2).py
@@ -7,11 +7,45 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1].values
+X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
+#Fitting linear regression to datasets
+from sklearn.linear_model import LinearRegression
+lin_reg= LinearRegression()
+lin_reg.fit(X, y)
 
+#Fitting polynomial regression to datasets
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+#Visualizing linear regression result
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title('Truth or bluff(Liner_Regression)') 
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+#Viualizing polynomial regression result
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid),1)
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color='blue')
+plt.title('Truth or bluff(Liner_Regression)') 
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+#Predicting slaary according to linear regression
+lin_reg.predict(6.5)
+
+#Predicting sslaary according to linear regression
+lin_reg_2.predict(poly_reg.fit_transform(10))
